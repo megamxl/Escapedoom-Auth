f575a62ebe79f2f7530685f58cb69d7e8cb38403
Thomas Meier <thommy2002@outlook.com>
2023-05-24 12:35:14 +0200

    Added more Node Infos

--- LectorPotral/src/main/java/com/escapedoom/auth/data/dataclasses/models/escaperoom/nodes/NodeType.java
+++ LectorPotral/src/main/java/com/escapedoom/auth/data/dataclasses/models/escaperoom/nodes/NodeType.java
@@ -3,7 +3,6 @@ package com.escapedoom.auth.data.dataclasses.models.escaperoom.nodes;
 public enum NodeType {
     Console,
     Details,
-
     Data,
     Zoom
 }
--- LectorPotral/src/main/java/com/escapedoom/auth/data/dataclasses/models/escaperoom/nodes/console/DataNodeInfo.java
+++ LectorPotral/src/main/java/com/escapedoom/auth/data/dataclasses/models/escaperoom/nodes/console/DataNodeInfo.java
@@ -13,5 +13,13 @@ public class DataNodeInfo extends NodeInfoAbstract {
 
     private Long outputID;
 
+    private String title;
+
+    private String desc;
+
+    private String parameterType;
+
+    private String exampleOutput;
+
     private String codeSnipped;
 }
